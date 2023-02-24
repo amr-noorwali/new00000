f9aa3ac890942a6cd9b10cc1dd403c2007108c38
amr-noorwali <113013055+amr-noorwali@users.noreply.github.com>
2023-02-24 17:52:14 +0300

    Add files via upload

---
+++ src/main/java/com/mycompany/mavenproject/Mavenproject.java
@@ -0,0 +1,87 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author aa_mm
+ */
+public class Mavenproject {
+
+    public static void main(String[] args) {
+        
+        Scanner me = new Scanner(System.in);{
+        System.out.print("inter number: ");
+        int x=me.nextInt();
+                
+        while(x<=100){
+            System.out.println("x= "+x);
+            x*=10;
+        }
+        
+       
+            
+    }
+        
+        
+            
+            
+        
+           
+           
+               
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+           
+
+                
+                
+                    
+        }
+    }
+
